dc56d2af598259a9a16fcb24e573c3a8b062072e
free02ike <164166214+free02ike@users.noreply.github.com>
2024-06-25 18:56:27 +0900

    課題アップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+ </head>
+ 
+ <body>
+     <p>
+     <?php
+     // 連想配列を作成
+     $vegetable = array(
+     "name" => "onion",
+     "price" => 200,
+     "weight" => 160
+ );
+
+     // 連想配列を出力
+     print_r($vegetable);
+?>
+         </p>
+     </body>
+     
+     </html>
